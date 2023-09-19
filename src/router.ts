7e00ea25f57ba690c27f654174ef0a9ab030c423
arisa2545 <arisa.ice@odds.team>
2023-09-19 23:35:19 +0700

    remove undefined

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,7 @@ import indexController from "./controller/indexController";
 import cors from '@fastify/cors'
 
 export default async function router(fastify: FastifyInstance) {
-  fastify.register(cors, undefined)
+  fastify.register(cors)
   fastify.register(userController, { prefix: "/api/v1/user" });
   fastify.register(indexController, { prefix: "/" });
 }
