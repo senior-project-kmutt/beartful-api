05026c8ca34c15c414a30679a2809d00f2909e7e
arisa2545 <68777796+arisa2545@users.noreply.github.com>
2023-09-19 23:41:17 +0700

    Allow Cors 🍺✨ (#3)
    * [ICE] add ignore file
    
    * [ICE] allow cors
    
    * remove undefined

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
+import cors from '@fastify/cors'
 import userController from "./controllers/userController";
 import indexController from "./controllers/indexController";
 
 export default async function router(fastify: FastifyInstance) {
+  fastify.register(cors)
   fastify.register(userController, { prefix: "/api/v1/user" });
   fastify.register(indexController, { prefix: "/" });
 }
