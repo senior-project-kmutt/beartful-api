a60582e87dd3ef3f44e90e37012a057bef6183fb
mottdy <mottdy22813@gmail.com>
2023-11-22 12:36:48 +0700

    api artwofk with pagination

--- src/controllers/artworkController.ts
+++ src/controllers/artworkController.ts
@@ -2,18 +2,29 @@ import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import { Artworks } from "../models/artwork";
 
 export default async function artworkController(fastify: FastifyInstance) {
-    // GET /api/v1/artwork
-    fastify.get("/", async function (
-        request: FastifyRequest,
-        reply: FastifyReply
-    ) {
-        console.log("get artwork");
-        const artworks = await getArtwork()
-        reply.send(artworks);
-    });
-
-    const getArtwork = async () => {
-        const artworks = await Artworks.find().lean()
-        return artworks
+  fastify.get(
+    "/",
+    async function (request: FastifyRequest, reply: FastifyReply) {
+      const { page, pageSize } = request.query as {
+        page: string;
+        pageSize: string;
+      };
+      const artworks = await getArtwork(page, pageSize);
+      reply.send(artworks);
     }
+  );
+
+  const getArtwork = async (page?: string, pageSize?: string) => {
+    const pages = page ? parseInt(page) : 1;
+    const pageSizes = pageSize ? parseInt(pageSize) : 30;
+    const skip = (pages - 1) * pageSizes;
+
+    try {
+      const artworks = await Artworks.find().skip(skip).limit(pageSizes).exec();
+      return artworks;
+    } catch (error) {
+      console.error("Error fetching artworks:", error);
+      throw error;
+    }
+  };
 }
