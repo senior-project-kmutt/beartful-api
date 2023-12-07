7bc0cfdfc10bd352c55071c2a79f6020ed25f840
mottdy <mottdy22813@gmail.com>
2023-12-07 12:54:36 +0700

    filter artwork vy type

--- src/controllers/artworkController.ts
+++ src/controllers/artworkController.ts
@@ -5,23 +5,25 @@ export default async function artworkController(fastify: FastifyInstance) {
   fastify.get(
     "/",
     async function (request: FastifyRequest, reply: FastifyReply) {
-      const { page, pageSize } = request.query as {
+      const { page, pageSize, type } = request.query as {
         page: string;
         pageSize: string;
+        type: string;
       };
-      const artworks = await getArtwork(page, pageSize);
+      const artworks = await getArtwork(page, pageSize, type);
       reply.send(artworks);
     }
   );
 
-  const getArtwork = async (page?: string, pageSize?: string) => {
+  const getArtwork = async (page?: string, pageSize?: string, type?:string) => {
     const pages = page ? parseInt(page) : 1;
     const pageSizes = pageSize ? parseInt(pageSize) : 30;
     const skip = (pages - 1) * pageSizes;
 
     try {
-      const artworks = await Artworks.find().skip(skip).limit(pageSizes).exec();
-      return artworks;
+      const artworks = await Artworks.find({ type: type}).skip(skip).limit(pageSizes).exec();
+      const shuffledArtworks = artworks.sort(() => Math.random() - 0.5);
+      return shuffledArtworks;
     } catch (error) {
       console.error("Error fetching artworks:", error);
       throw error;
