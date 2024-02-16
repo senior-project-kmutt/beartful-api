c17cb663ae7dd6b6c283c85cc19c245401e0bde9
mottdy <mottdy22813@gmail.com>
2024-02-16 20:07:58 +0700

    handle error status categories

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -1,12 +1,18 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import { getCategory } from "../services/categoryService";
+import { ErrorCode } from "../response/errorResponse";
 
 export default async function categoryController(fastify: FastifyInstance) {
     fastify.get(
         "/",
         async function (request: FastifyRequest, reply: FastifyReply) {
-            const categories = await getCategory();
-            reply.send(categories);
+            try {
+                const categories = await getCategory();
+                return reply.status(200).send(categories);
+            } catch (error) {
+                return reply.status(500).send(ErrorCode.InternalServerError);
+            }
         }
     );
+
 }
