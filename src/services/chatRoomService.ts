9a2672a5f3e97ef519cb78e18e88cd3cf06c8961
arisa2545 <68777796+arisa2545@users.noreply.github.com>
2024-03-03 00:44:00 +0700

    API for create chat room ✨ (#17)
    * API for create new chatRoom and getChatRoomById
    
    * fixed check existing chat room
    
    * handle not found user

--- src/services/chatRoomService.ts
+++ src/services/chatRoomService.ts
@@ -1,10 +1,21 @@
 import { ChatRoom } from "../models/chatRoom";
 import { ChatMessages } from "../models/chatMessages";
+import { IUsers, Users } from "../models/user";
 
 export const getAllChatRoom = async () => {
   try {
     const chatRooms = await ChatRoom.find();
     console.log("Chat Rooms:", chatRooms);
+    return chatRooms;
+  } catch (error) {
+    console.error("Error fetching chat rooms:", error);
+    throw error;
+  }
+};
+
+export const getChatRoomById = async (chatRoomId: string) => {
+  try {
+    const chatRooms = await ChatRoom.findOne({ _id: chatRoomId });
     return chatRooms;
   } catch (error) {
     console.error("Error fetching chat rooms:", error);
@@ -20,4 +31,35 @@ export const getMessagesByChatRoom = async (chatRoomId: string) => {
 export const getLastMessagesByChatRoom = async (chatRoomId: string) => {
   const chatMessgaes = await ChatMessages.find().where("chat_room_id").equals(chatRoomId).sort({ createdAt: 'desc' })
   return chatMessgaes[0]
+}
+
+export const createChatRoom = async (participants: string[]) => {
+  const newParticipants = await transformPaticipants(participants);
+  const existingChatRoom = await ChatRoom.findOne({
+    participants: { $all: newParticipants }
+  })
+  if (existingChatRoom) {
+    return existingChatRoom
+  } else {
+    const newChatRoom = new ChatRoom({
+      participants: newParticipants
+    });
+    await newChatRoom.validate();
+    const response = await newChatRoom.save();
+    return response
+  }
+}
+
+const transformPaticipants = async (participants: string[]) => {
+  const transformPaticipants: string[] = []
+  const participantPromises = participants.map(async participant => {
+    const user: IUsers = await Users.findOne({ username: participant });
+    if (!user) {
+      throw new Error(`User '${participant}' not found`);
+    }
+    const userId = String(user._id);
+    transformPaticipants.push(userId);
+  })
+  await Promise.all(participantPromises);
+  return transformPaticipants
 }
