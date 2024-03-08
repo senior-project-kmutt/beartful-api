a42991e552624b7e1be84a44a923b1d4c0c24249
mottdy <mottdy22813@gmail.com>
2024-03-08 21:51:34 +0700

    transfer api

--- src/controllers/accountController.ts
+++ src/controllers/accountController.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import { IBankAccountTransfer } from "../models/payment";
-
+import { ErrorCode } from "../response/errorResponse";
+import { getUserById, validateToken } from "../services/userService";
 
 const omise = require("omise")({
     secretKey: "skey_test_5x1jr0hpfmne5jj68on"
@@ -46,15 +47,46 @@ export default async function accountController(fastify: FastifyInstance) {
         request: FastifyRequest,
         reply: FastifyReply
     ) {
-        try {
-            // const { email, name, type, bank_account } = request.body as IBankAccountTransfer;
-            const transfer = await omise.transfers.create({
-                amount: '140000',
-                recipient: 'recp_test_5xf7lugyuikhxlwgfjc',
-            });
-            reply.send(transfer)
-        } catch (err) {
-            reply.send(err);
+        const auth = request.headers.authorization;
+        const { amount } = request.body as { amount: string };
+
+        if (auth) {
+            try {
+                const decode = validateToken(auth);
+                if (!decode.id) {
+                    return reply.status(401).send(ErrorCode.Unauthorized);
+                }
+                if (decode.role != "freelance") {
+                    return reply.status(401).send(ErrorCode.Unauthorized);
+                }
+                const user = await getUserById(decode.id)
+                const transfer = await omise.transfers.create({
+                    amount: amount,
+                    recipient: user.recipient,
+                });
+                return reply.status(200).send(transfer);
+            } catch (error: any) {
+                return reply.status(401).send(ErrorCode.Unauthorized);
+            }
+        } else {
+            return reply.status(500).send(ErrorCode.InternalServerError);
         }
     });
+
+     // POST /api/freelance
+    //  fastify.post("/transfer", async function (
+    //     request: FastifyRequest,
+    //     reply: FastifyReply
+    // ) {
+    //     try {
+    //         // const { email, name, type, bank_account } = request.body as IBankAccountTransfer;
+    //         const transfer = await omise.transfers.create({
+    //             amount: '140000',
+    //             recipient: 'recp_test_5xf7lugyuikhxlwgfjc',
+    //         });
+    //         reply.send(transfer)
+    //     } catch (err) {
+    //         reply.send(err);
+    //     }
+    // });
 }
